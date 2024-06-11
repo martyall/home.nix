dfaa654bd45a41a62f82a5207fbb856339d8abfe
Martin Allen <31280145+martyall@users.noreply.github.com>
2024-06-11 08:41:26 -0700

    Update home.nix

--- home.nix
+++ home.nix
@@ -7,15 +7,31 @@
   home.homeDirectory = "/home/martin";
 
   home.packages = [
-    pkgs.tmux
-    pkgs.neovim
-    pkgs.htop
-    pkgs.nix-output-monitor
-    pkgs.jq
-    pkgs.nethogs
-    pkgs.xclip
+    pkgs.cbc
+    pkgs.cvc4
+    pkgs.docker-compose
     pkgs.git
+    pkgs.htop
+    pkgs.jq
+    pkgs.m4
+    pkgs.neovim
+    pkgs.nethogs
     pkgs.netcat
+    pkgs.nix-output-monitor
+    pkgs.pdftk
+    # pkgs.python3
+    # pkgs.rustup
+    pkgs.silver-searcher
+    pkgs.tree
+    pkgs.tmate
+    pkgs.tmux
+    pkgs.tmuxinator
+    pkgs.which
+    pkgs.vscode
+    pkgs.xclip
+    pkgs.yarn
+    pkgs.nodejs
+    pkgs.stack
   ];
 
   # This value determines the Home Manager release that your
@@ -26,20 +42,35 @@
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "22.05";
+  home.stateVersion = "23.05";
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+  nixpkgs.config.allowUnfree = true;
 
-  programs.direnv.enable = true;
-  programs.direnv.nix-direnv.enable = true;
+  programs.tmux = {
+    enable = true;
+    keyMode = "vi";
+    tmuxinator.enable = true;
+    extraConfig = ''
+      set-option -g mouse on
+      set-option -g default-shell ''${SHELL}
+    '';
+  };
 
   programs.zsh = {
     enable = true;
     shellAliases = {
       pbcopy ="xclip -selection clipboard";
       pbpaste = "xclip -selection clipboard -o";
+      ls = "ls -l --color";
     };
+    initExtra = ''
+      source $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh
+      . /home/martin/.ghcup/env
+      PATH=/home/martin/.cargo/bin:$PATH
+      PATH=/home/martin/.foundry/bin:$PATH
+    '';
     oh-my-zsh = {
       enable = true;
       plugins = [ "git" ];
@@ -47,11 +78,27 @@
     };
   };
 
+  programs.go.enable = true;
 
   programs.git = {
     enable = true;
     userName = "martyall";
     userEmail = "martyall@protonmail.com";
+  };
+
+  programs.vscode = {
+    enable = true;
+    extensions = with pkgs.vscode-extensions; [
+      dracula-theme.theme-dracula
+      vscodevim.vim
+      yzhang.markdown-all-in-one
+    ];
+  };
+
+  programs.direnv = {
+      enable = true;
+      enableZshIntegration = true; # see note on other shells below
+      nix-direnv.enable = true;
   };
 
 }
